4bd240c92b1677caed59472fb486e97cd59cfcb2
hlg <public.void@gmx.de>
2020-07-15 09:43:46 +0200

    Express-EMF rules for aggregation attributes

--- ExpressEMF/src/org/emoflon/ibex/tgg/run/expressemf/config/DemoclesRegistrationHelper.java
+++ ExpressEMF/src/org/emoflon/ibex/tgg/run/expressemf/config/DemoclesRegistrationHelper.java
@@ -1,13 +1,10 @@
 package org.emoflon.ibex.tgg.run.expressemf.config;
 
 import de.htwdd.expressEMF.express.impl.ExpressPackageImpl;
-import de.htwdd.expressEMF.express.AttributeDecl;
-import de.htwdd.expressEMF.express.EntityDecl;
 import de.htwdd.expressEMF.express.ExpressPackage;
+
 import java.io.IOException;
 
-import org.eclipse.emf.ecore.EDataType;
-import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.impl.EcorePackageImpl;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -24,7 +21,7 @@ public class DemoclesRegistrationHelper implements IRegistrationHelper {
 	public void registerMetamodels(ResourceSet rs, IbexExecutable executable) throws IOException {
 		ExpressPackageImpl.init();
 		EcorePackageImpl.init();
-		rs.getPackageRegistry().put("platform:/resource/ExpressParser/model/generated/Express.ecore", ExpressPackage.eINSTANCE);
+		rs.getPackageRegistry().put("platform:/resource/ExpressParser/model/custom/Express.ecore", ExpressPackage.eINSTANCE);
 		rs.getPackageRegistry().put("platform:/plugin/org.eclipse.emf.ecore/model/Ecore.ecore", EcorePackage.eINSTANCE);
 		// Replace to register generated code or handle other URI-related requirements
 	}
@@ -37,9 +34,15 @@ public class DemoclesRegistrationHelper implements IRegistrationHelper {
 		options.project.path("ExpressEMF");
 		options.debug.ibexDebug(true);
 		options.patterns.lookAheadStrategy(FilterNACStrategy.NONE);
-		options.patterns.ignoreInjectivity((EntityDecl,EClass)->true);
+		options.patterns.ignoreInjectivity((EntityTypeDecl,EClass)->true);
+		options.patterns.ignoreInjectivity((TypeDecl,EClass)->true);
+		options.patterns.ignoreInjectivity((NamedTypeDecl,EClass)->true);
 		options.patterns.ignoreInjectivity((AttributeDecl, EStructuralFeature)->true);
-		options.patterns.ignoreInjectivity((SimpleDataType, EDataType)->true);
+		
+		options.patterns.ignoreInjectivity((SimpleTypes, EDataType)->true);
+		options.patterns.ignoreInjectivity((StringType, EDataType)->true);
+		options.patterns.ignoreInjectivity((IntegerType, EDataType)->true);
+		
 		options.csp.userDefinedConstraints(new UserDefinedRuntimeTGGAttrConstraintFactory());
 		options.registrationHelper(this);
 		return options;
